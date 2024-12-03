a56e8c32bd4dcb28a09c653e4fe3770156c10f3d
SenpaiCha <54840053+SenpaiCha@users.noreply.github.com>
2024-12-03 16:09:36 +0700

    Test uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -19,13 +19,13 @@ const workerConfig = {
     // Example HTTP Monitor
     {
       id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
+      name: 'Test',
       // `method` should be `TCP_PING` for tcp monitors
       method: 'TCP_PING',
       // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
+      target: '23.168.86.140',
       tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
+      statusPageLink: '',
       timeout: 5000,
     },
   ],
